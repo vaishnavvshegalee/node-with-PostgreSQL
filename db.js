a574de4c736f2a41fae4d80d8b99e4add18756cd
VAISHNAV SHEGALE <vaishnavshegalee>
2024-11-05 07:21:16 +0530

    set env variables

--- db.js
+++ db.js
@@ -1,11 +1,17 @@
  const { Pool } = require('pg');
-
+ const {config} = require('dotenv');
+ config();
  const pool = new Pool({
-    user:'postgres',
-    host:'localhost',
-    database:'students',
-    password:'test',
-    port:'5432'
+    user:process.env.USER,
+    host:process.env.HOST,
+    database:process.env.DATABASE,
+    password:process.env.PASSWORD,
+    port:process.env.D_PORT
  });
 
+ pool.connect().then(()=>{
+   console.log('Databse Connected');
+ }).catch((err)=>{
+   throw Error('Connection error: ',err.message)
+ });
  module.exports = pool;
